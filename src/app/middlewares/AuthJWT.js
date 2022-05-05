7934098bf60c8e53c14578288cd480da9020b96d
Nguyen Hoang Linh <linh072217@gmail.com>
2022-05-05 15:28:58 +0700

    chore: add middle ware check roles

--- src/app/middlewares/AuthJWT.js
+++ src/app/middlewares/AuthJWT.js
@@ -1,5 +1,11 @@
 const jwt = require("jsonwebtoken");
 const authConfig = require("../config/auth.config");
+const db = require("../models");
+const ROLES = {
+  ADMIN : "admin",
+  MODERATOR: "moderator",
+  USER : "user"
+}
 const verifyToken = (req, res, next)=>{
   let token = req.cookies.accessToken;
   if(!token){
@@ -13,6 +19,37 @@ const verifyToken = (req, res, next)=>{
     next();
   });
 } 
+const isAdminOrModerator = async (req, res, next)=>{
+  try{
+    let userId = req.userId || null;
+    let user = await db.User.findOne({
+      where: {
+        id : userId
+      }
+    }).catch(err=>{throw err})
+    let roles = await user.getRoles();
+    let valid = false;
+    console.log(roles)
+    roles.forEach(role=>{
+      console.log(role.name)
+      if(role.name == ROLES.ADMIN || role.name == ROLES.MODERATOR){
+        console.log(role.name)
+        valid = true;
+      }
+    })
+    console.log(valid)
+    if(valid){
+      next();
+    }
+    else
+      res.status(403).json({status:"failed", message:"you do not have privilege"});
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json({status:"success", message:"server has an err"})
+  }
+}
 module.exports = {
-  verifyToken
+  verifyToken,
+  isAdminOrModerator
 }
